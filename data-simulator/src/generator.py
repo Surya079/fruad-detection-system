4b5472a2890da1491cede5b8eb8290e68eef0dae
Surya079 <developer.surya03@gmail.com>
2026-02-28 17:31:34 +0530

    created fake generated using python code

---
+++ data-simulator/src/generator.py
@@ -0,0 +1,12 @@
+import uuid
+import random
+import time 
+import requests
+from datetime import datetime
+
+NIFI_ENDPOINTS = "http://localhost:8443/transactions"
+
+def generate_transactions():    
+    return {
+        
+    }
