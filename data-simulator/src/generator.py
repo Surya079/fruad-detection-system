8ce04cc273cf7a1a4717a0d7c4d6a8f443f7a152
Surya079 <developer.surya03@gmail.com>
2026-03-01 19:07:30 +0530

    spark job created using python and docker compose modifed

--- data-simulator/src/generator.py
+++ data-simulator/src/generator.py
@@ -1,12 +1,36 @@
 import uuid
 import random
-import time 
+import time
+from datetime import datetime, timezone
 import requests
-from datetime import datetime
 
-NIFI_ENDPOINTS = "http://localhost:8443/transactions"
+# Use port 8082 (HTTP) - this goes to your processor
+NIFI_ENDPOINTS = "http://localhost:8081/transactions"
 
 def generate_transactions():    
     return {
-        
+        "transaction_id": str(uuid.uuid4()),
+        "customer_id": f"C{random.randint(1000, 9999)}",
+        "amount": round(random.uniform(10, 50000),2 ),
+        "currency": "INR",
+        "merchant_id": f"M{random.randint(100, 999)}",
+        "merchant_category": random.choice(["ECOMMERCE", "ATM", "POS"]),
+        "transaction_type": random.choice(["ONLINE", "ATM", "POS"]),
+        "transaction_time": datetime.now(timezone.utc).isoformat(),
+        "country": "IN",
+        "device_id": f"device-{random.randint(1, 50)}"
     }
+
+while True:
+    txn = generate_transactions()
+    try:
+        response = requests.post(
+            NIFI_ENDPOINTS, 
+            json=txn, 
+            timeout=5,
+            headers={'Content-Type': 'application/json'}
+        )
+        print(f"✅ Sent: {txn['transaction_id']} - Status: {response.status_code}")
+    except Exception as e:
+        print(f"❌ Error: {e}")
+    time.sleep(1)
